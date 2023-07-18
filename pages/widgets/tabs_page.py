467ff102d9ab5fc14d8464ca42ebd774f160c9d0
Vladyslav Opanasenko <vla.opanasenko@gmail.com>
2023-07-18 10:56:43 +0300

    added tabs_page.py and test_tabs.py

---
+++ pages/widgets/tabs_page.py
@@ -0,0 +1,43 @@
+from selenium.webdriver.common.by import By
+
+from pages.base_page import BasePage
+
+
+class TabsPage(BasePage):
+    TABS_WHAT = [By.CSS_SELECTOR, "a[id='demo-tab-what']"]
+    TABS_WHAT_CONTENT = [By.CSS_SELECTOR, "div[id='demo-tabpane-what']"]
+    TABS_ORIGIN = [By.CSS_SELECTOR, "a[id='demo-tab-origin']"]
+    TABS_ORIGIN_CONTENT = [By.CSS_SELECTOR, "div[id='demo-tabpane-origin']"]
+    TABS_USE = [By.CSS_SELECTOR, "a[id='demo-tab-use']"]
+    TABS_USE_CONTENT = [By.CSS_SELECTOR, "div[id='demo-tabpane-use']"]
+    TABS_MORE = [By.CSS_SELECTOR, "a[id='demo-tab-more']"]
+    TABS_MORE_CONTENT = [By.CSS_SELECTOR, "div[id='demo-tabpane-more']"]
+
+    def check_tabs(self, name_tab):
+        tabs = {
+            'what_tab':
+                {
+                    'title': self.TABS_WHAT,
+                    'content': self.TABS_WHAT_CONTENT
+                },
+            'origin_tab':
+                {
+                    'title': self.TABS_ORIGIN,
+                    'content': self.TABS_ORIGIN_CONTENT
+                },
+            'use_tab':
+                {
+                    'title': self.TABS_USE,
+                    'content': self.TABS_USE_CONTENT
+                },
+            'more_tab':
+                {
+                    'title': self.TABS_MORE,
+                    'content': self.TABS_MORE_CONTENT
+                }
+        }
+
+        tab = self.element_is_visible(tabs[name_tab]['title'])
+        tab.click()
+        tab_content = self.element_is_visible(tabs[name_tab]['content']).text
+        return tab.text, len(tab_content)
---
+++ tests/test_widgets/test_tabs.py
@@ -0,0 +1,19 @@
+from pages.widgets.tabs_page import TabsPage
+
+
+class TestTabs:
+    page_link = 'https://demoqa.com/tabs'
+
+    def test_slider(self, driver):
+        tabs_page = TabsPage(driver, self.page_link)
+        tabs_page.open()
+
+        what_tab, what_content = tabs_page.check_tabs('what_tab')
+        origin_tab, origin_content = tabs_page.check_tabs('origin_tab')
+        use_tab, use_content = tabs_page.check_tabs('use_tab')
+        more_tab, more_content = tabs_page.check_tabs('more_tab')
+
+        assert what_tab == 'What' and what_content != 0, "The tab 'What' was not pressed or the text is missing"
+        assert origin_tab == 'Origin' and origin_content != 0, "The tab 'Origin' was not pressed or the text is missing"
+        assert use_tab == 'Use' and use_content != 0, "The tab 'Use' was not pressed or the text is missing"
+        assert more_tab == 'More' and more_content != 0, "The tab 'More' was not pressed or the text is missing"
